aeade3733a20558f5aeaaf84476792cb7786dd26
hyejun8701 <hyejun8701@gmail.com>
2019-04-05 13:49:54 +0900

    Context.xml bean 설정과 이해

---
+++ BoardWeb/src/main/java/com/springbook/biz/TVUser.java
@@ -0,0 +1,24 @@
+package com.springbook.biz;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TVUser {
+
+	public static void main(String[] args) {
+		// 1. Spring 컨테이너 구동
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		// 2. Spring 컨테이너로 부터 필요한 객체 요청(lookup)
+		TV tv = (TV) factory.getBean(TV.class);
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+		
+		// 3. Spring 컨테이너 종료
+		factory.close();
+		
+	}
+
+}
